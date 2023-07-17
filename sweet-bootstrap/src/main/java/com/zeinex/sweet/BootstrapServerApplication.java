9f427bde74240d2984c9ff833d86ff3c3896aa60
未小雨 <fansty_like@hotmail.com>
2023-07-17 23:09:26 +0800

    初步完成gradle工程设置

---
+++ sweet-bootstrap/src/main/java/com/zeinex/sweet/BootstrapServerApplication.java
@@ -0,0 +1,19 @@
+package com.zeinex.sweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 引导启动程序
+ *
+ * @author 未小雨
+ * @date 2021/6/24 12:24
+ */
+@SpringBootApplication
+public class BootstrapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapServerApplication.class, args);
+    }
+
+}
