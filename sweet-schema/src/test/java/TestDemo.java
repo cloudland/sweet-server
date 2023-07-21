c9c95ff89c2a06f46023ac1348c9644543fb335f
未小雨 <fansty_like@hotmail.com>
2023-07-21 15:14:10 +0800

    初步完成gradle工程设置

---
+++ sweet-schema/src/test/java/TestDemo.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * @author 未小雨
+ * @date 2023/7/19 20:33
+ */
+public class TestDemo {
+
+    @Test
+    public void test() {
+
+        System.out.println("=== junit ===");
+
+    }
+
+}
